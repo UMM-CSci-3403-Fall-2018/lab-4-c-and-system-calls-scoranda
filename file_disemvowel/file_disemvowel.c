720289c5fe7f12c2f6f6dbc8070d7a2e3a2bd144
Avery Koranda <koran023@morris.umn.edu>
2018-10-04 11:14:29 -0500

    Add file_disemvowel and start implementing disemvowel and is_vowel functions

---
+++ file_disemvowel/file_disemvowel.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define BUF_SIZE 1024
+
+// from Avery/Leah Lab 2: https://github.com/UMM-CSci-3403-Fall-2018/lab-2-memory-management-in-c-wild-cats/blob/master/disemvowel/disemvowel.c
+bool is_vowel(char c) {
+    /*
+     * Returns true if c is a vowel (upper or lower case), and
+     * false otherwise.
+     */
+  char *vowels = (char*) calloc(11, sizeof(char));
+  vowels[0] = 'a';
+  vowels[1] = 'e';
+  vowels[2] = 'i';
+  vowels[3] = 'o';
+  vowels[4] = 'u';
+  vowels[5] = 'A';
+  vowels[6] = 'E';
+  vowels[7] = 'I';
+  vowels[8] = 'O';
+  vowels[9] = 'U';
+  vowels[10] = '\0';
+
+  // referenced: https://stackoverflow.com/questions/15094834/check-if-a-value-exist-in-a-array
+  bool isContained = false;
+  int i;
+  for (i=0; i < 10; i++) {
+   if(vowels[i] == letter){
+    isContained = true;
+     }
+  }
+  free(vowels);
+  return isContained;
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+    /*
+     * Copy all the non-vowels from in_buf to out_buf.
+     * num_chars indicates how many characters are in in_buf,
+     * and this function should return the number of non-vowels that
+     * that were copied over.
+     */
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+    /*
+     * Copy all the non-vowels from inputFile to outputFile.
+     * Create input and output buffers, and use fread() to repeatedly read
+     * in a buffer of data, copy the non-vowels to the output buffer, and
+     * use fwrite to write that out.
+     */
+
+     char* in_buf = (char*) calloc(BUF_SIZE, sizeof(char));
+     char* out_buf = (char*) calloc(BUF_SIZE, sizeof(char));
+
+     int in_len = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+     while (in_len != 0) {
+       int non_vowels = copy_non_vowels(strlen(in_buf), in_buf, out_buf);
+       fwrite(out_buf, sizeof(char), non_vowels, outputFile);
+       in_len = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+     }
+
+}
+
+int main(int argc, char *argv[]) {
+    FILE *inputFile;
+    FILE *outputFile;
+
+    // Code that processes the command line arguments
+    // and sets up inputFile and outputFile.
+
+    disemvowel(inputFile, outputFile);
+
+    return 0;
+}
